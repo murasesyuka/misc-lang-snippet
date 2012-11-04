6f3a401b14ff7bcdfc66ef7dba7575bbc19f247f
murase_syuka <murase.syuka+github@gmail.com>
2012-11-04 18:06:13 +0900

    make all routes of '/'.
    HTTP methods paired with a URL-matching pattern.

--- ruby/sinatra/myapp.rb
+++ ruby/sinatra/myapp.rb
@@ -3,3 +3,25 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+post '/' do
+  # .. create something
+end
+
+put '/' do
+  # .. replace something
+end
+
+patch '/' do
+  # .. modify something
+end
+
+delete '/' do
+  # .. annihilate something
+end
+
+options '/' do
+  # .. appease something
+end
+
+

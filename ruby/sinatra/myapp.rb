2f4354c823eab0a0919bb3325a862d68034cc009
murase_syuka <murase.syuka+github@gmail.com>
2012-11-13 07:41:26 +0900

    add : For a condition that takes multiple values use a splat

--- ruby/sinatra/myapp.rb
+++ ruby/sinatra/myapp.rb
@@ -34,6 +34,22 @@ get '/win_a_car' do
   "Sorry, you lost"
 end
 
+set(:auth) do |*roles|   # <- notice the splat here
+  condition do
+    unless logged_in? && roles.any? {|role| current_user.in_role? role }
+      redirect "/login/", 303
+    end
+  end
+end
+
+get '/my/account/', :auth => [:user, :admin] do
+  "Your Account Details"
+end
+
+get '/only/admin/', :auth => :admin do
+  "Only admins are allowed here!"
+end
+
 get '/foo' do
   # Matches non-songbird browsers
   "You're not using Songbird"

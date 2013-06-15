446e9a77bbc506ba11e3c5af60574dfc66e1865d
murase_syuka <murase.syuka+github@gmail.com>
2013-06-15 17:00:55 +0900

    step2 fake it

--- ruby/testunit/mystack.rb
+++ ruby/testunit/mystack.rb
@@ -2,4 +2,11 @@ class MyStack
   def empty?
     true
   end
+
+  def push(v)
+  end
+
+  def pop
+    3
+  end
 end
--- ruby/testunit/test_mystack.rb
+++ ruby/testunit/test_mystack.rb
@@ -9,4 +9,9 @@ class TestMyStack < Test::Unit::TestCase
   def test_empty_by_empty_myarray
     assert(@stack.empty?,"empty error")
   end
+
+  def test_push_and_pop_myarray
+    @stack.push(3)
+    assert_equal(@stack.pop, 3)
+  end
 end

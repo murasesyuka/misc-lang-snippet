767a43f89ea421e16e708a80add3a35d29601474
murase_syuka <murase.syuka+github@gmail.com>
2013-10-26 16:05:12 +0900

    fix : try catch exception code

--- python/echo_server.py
+++ python/echo_server.py
@@ -6,9 +6,11 @@ s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 try:
     os.unlink("/tmp/socket")
-except OSError:
-    pass 
+except OSError as e:
+    if e.errno != errno.ENOENT:
+        raise 
 
+    
 s.bind("/tmp/socket")
 s.listen(1)
 conn, addr = s.accept()

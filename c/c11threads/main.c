56be5535ae2e8697e07c388044d1a8364aa7c0d1
murase_syuka <murase.syuka+github@gmail.com>
2015-04-12 23:19:03 +0900

    test mtx_lock() and mtx_unlock() API

--- c/c11threads/main.c
+++ c/c11threads/main.c
@@ -3,13 +3,19 @@
 #define HAVE_PTHREAD
 #include "c11/threads.h"
 
+mtx_t mtx;
+
 int func(void *arg)
 {
 	//int n = (long)arg;
 	//xtime dur = { 4, 0};
 
-	printf("%ld\n", (unsigned long int)thrd_current());
+	mtx_lock(&mtx);
+	
+	printf("thrd_current()vim is %ld\n", (unsigned long int)thrd_current());
 
+	mtx_unlock(&mtx);
+	
 	return 0;
 }
 
@@ -18,7 +24,8 @@ int main()
 	int i;
 	enum {SIZE = 4};
 	thrd_t thr_s[SIZE];
-	
+
+	mtx_init(&mtx, mtx_plain);
 	for(i=0; i<SIZE; i++) {
 		thrd_create(&thr_s[i], func, (void*)(long)i);
 	}

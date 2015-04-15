f509a3c94bcde6c2544cad9e8ac7a6a30565754d
murase_syuka <murase.syuka+github@gmail.com>
2015-04-15 18:07:21 +0900

    impl Stack, not mutex

--- c/c11threads/main.c
+++ c/c11threads/main.c
@@ -1,35 +1,67 @@
 #include <stdio.h>
 #include <threads.h>
 
-mtx_t mtx;
+enum{S_SIZE = 10};
+typedef struct Stack{
+	int v_[S_SIZE];
+	/* int vsize_; */
+	int vused_;
+	mtx_t *mtx_;
+} Stack10;
+
+int push(Stack10 *s, int x){
+	if(s->vused_ == S_SIZE){
+		return -1;	/* Full */
+	}
+	s->v_[s->vused_++] = x;
+	return 0;
+}
+
+int pop(Stack10 *s, int *x){
+	if(s->vused_ == 0){
+		return -1;
+	}
+	*x = s->v_[--s->vused_];
+	return 0;
+}
+
 
 int func(void *arg)
 {
-	//int n = (long)arg;
-	//xtime dur = { 4, 0};
-
-	mtx_lock(&mtx);
-	
+	int i;
+	Stack10 *s =(Stack10*)arg;
 	printf("thrd_current()vim is %ld\n", (unsigned long int)thrd_current());
-
-	mtx_unlock(&mtx);
-	
+	for(i = 0; i<3; ++i){
+		push(s, i);
+	}
 	return 0;
 }
 
 int main()
 {
-	int i;
+	int i, ret;
+	Stack10 s = {{0},0};
+	
+	mtx_t mtx;
+	
 	enum {SIZE = 4};
 	thrd_t thr_s[SIZE];
 
-	mtx_init(&mtx, mtx_plain);
-	for(i=0; i<SIZE; i++) {
-		thrd_create(&thr_s[i], func, (void*)(long)i);
+	ret = mtx_init(&mtx, mtx_plain);
+	if(ret != thrd_success){
+		return ret;
 	}
+	s.mtx_ = &mtx;
 	
 	for(i=0; i<SIZE; i++) {
+		thrd_create(&thr_s[i], func, (void*)(Stack10*)&s);
+	}
+	for(i=0; i<SIZE; i++) {
 		thrd_join(thr_s[i], 0);
+	}
+
+	for(i=0; i<S_SIZE; i++){
+		printf("staci[%d] = %d\n",i,s.v_[i]);
 	}
 	
 	return 0;

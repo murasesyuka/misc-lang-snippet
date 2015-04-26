a0177b3895ce9239d34fad49167a540c2ceafd13
murase_syuka <murase.syuka+github@gmail.com>
2015-04-26 22:35:53 +0900

    fix compile error & warning

--- c/pthread/memleak_forgetjoin.c
+++ c/pthread/memleak_forgetjoin.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<pthread.h>
 
-void run() {
+void* run() {
    pthread_exit(0);
 }
 
@@ -10,7 +10,8 @@ int main () {
    int rc;
    long count = 0;
    while(1) {
-      if(rc = pthread_create(&thread, 0, run, 0) ) {
+	   rc = pthread_create(&thread, 0, run, 0);
+      if ( rc ) {
          printf("ERROR, rc is %d, so far %ld threads created\n", rc, count);
          perror("Fail:");
          return -1;

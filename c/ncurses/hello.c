b363ae996136a8da345af3a0ac377ace50571063
murase_syuka <murase.syuka+github@gmail.com>
2015-09-13 22:03:56 +0900

    refactoring : for minimal code

--- c/ncurses/hello.c
+++ c/ncurses/hello.c
@@ -2,22 +2,24 @@
 
 int main()
 {
-	initscr();
-	
-	start_color();
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	bkgd(COLOR_PAIR(1));
+	initscr();// terminal start
+	{	
+		/* start_color(); */
+		/* init_pair(1, COLOR_WHITE, COLOR_BLACK); */
+		/* bkgd(COLOR_PAIR(1)); */
+		
+		/* erase(); */
+		
+		move(20, 50);
+		addstr("Hello Curses.");
+		
+		/* refresh(); */
+		
+		/* timeout(-1); */
 
-	erase();
-	
-	move(20, 50);
-	addstr("Hello Curses.");
-	refresh();
-
-	timeout(-1);
-	getch();
-
-	endwin();
+		getch();
+	}
+	endwin();// terminal end
 	
 	return 0;
 }

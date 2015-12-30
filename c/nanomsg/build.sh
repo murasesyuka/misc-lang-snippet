96a3428e5478e9461dcb82a9abc108af99599373
murase_syuka <murase.syuka+github@gmail.com>
2015-12-30 22:59:45 +0900

    copy

--- c/nanomsg/build.sh
+++ c/nanomsg/build.sh
@@ -1,2 +1,4 @@
-gcc -pthread pipeline.c -lnanomsg -o pipeline -lanl
-gcc -pthread reqrep.c -lnanomsg -o reqrep -lanl
+gcc -pthread pipeline.c -L/usr/local/lib -lnanomsg -o pipeline
+gcc -pthread reqrep.c -L/usr/local/lib -lnanomsg -o reqrep
+gcc -pthread pair.c -L/usr/local/lib -lnanomsg -o pair
+gcc -pthread pubsub.c -L/usr/local/lib -lnanomsg -o pubsub

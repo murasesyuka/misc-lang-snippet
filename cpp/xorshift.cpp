a58cf666aaf899e376be66ae51e3c241e9e1197b
murase_syuka <murase.syuka+github@gmail.com>
2016-01-08 20:03:23 +0900

    add : xorshift32

---
+++ cpp/xorshift.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <chrono>
+
+using uint32 = unsigned int;
+
+uint32 xorshift32()
+{
+	static uint32 y = 0x1234abcd;
+
+	y = y ^ (y << 13);
+	y = y ^ (y >> 17);
+	y = y ^ (y << 15);
+
+	return y;
+}
+
+template<typename F>
+void bench(const F& f)
+{
+	size_t N = 10000000;
+
+	auto start = std::chrono::system_clock::now();
+
+	for (int i = 0; i < N; i++) {
+		f();
+	}
+
+	auto end = std::chrono::system_clock::now();
+
+	auto diff = end - start;
+
+	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() << std::endl;
+
+	return;
+}
+
+
+int main(int argc, char *argv[])
+{
+	bench( []{ xorshift32(); } );
+	return 0;
+}
+

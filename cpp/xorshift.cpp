31724cac80deddfaeffd596b26a941c4e2fcfcd7
murase_syuka <murase.syuka+github@gmail.com>
2016-01-08 20:33:29 +0900

    trivialfix

--- cpp/xorshift.cpp
+++ cpp/xorshift.cpp
@@ -3,11 +3,9 @@
 #include <random>
 #include <string>
 
-using uint32 = unsigned int;
-
-uint32 xorshift32()
+uint32_t xorshift32()
 {
-	static uint32 y = 0x1234abcd;
+	static uint32_t y = 0x1234abcd;
 
 	y = y ^ (y << 13);
 	y = y ^ (y >> 17);
@@ -16,16 +14,20 @@ uint32 xorshift32()
 	return y;
 }
 
-uint32_t xorshift128(void) { 
+uint32_t xorshift128()
+{ 
   static uint32_t x = 123456789;
   static uint32_t y = 362436069;
   static uint32_t z = 521288629;
   static uint32_t w = 88675123; 
+
   uint32_t t;
  
   t = x ^ (x << 11);
   x = y; y = z; z = w;
-  return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); 
+  w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+  
+  return w;
 }
 
 template<typename F>

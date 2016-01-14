5f4705f3b179b28e0e0721642332bcfdfd9383b8
murase_syuka <murase.syuka+github@gmail.com>
2016-01-14 23:51:37 +0900

    align indent

--- cpp/xorshift/xorshift.cpp
+++ cpp/xorshift/xorshift.cpp
@@ -16,18 +16,18 @@ uint32_t xorshift32()
 
 uint32_t xorshift128()
 { 
-  static uint32_t x = 123456789;
-  static uint32_t y = 362436069;
-  static uint32_t z = 521288629;
-  static uint32_t w = 88675123; 
-
-  uint32_t t;
- 
-  t = x ^ (x << 11);
-  x = y; y = z; z = w;
-  w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
-  
-  return w;
+	static uint32_t x = 123456789;
+	static uint32_t y = 362436069;
+	static uint32_t z = 521288629;
+	static uint32_t w = 88675123; 
+	
+	uint32_t t;
+	
+	t = x ^ (x << 11);
+	x = y; y = z; z = w;
+	w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+	
+	return w;
 }
 
 template<typename F>
